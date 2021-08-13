aff9ca46988ecc28ad47aa63e2c3120920b3b6f2
DESKTOP-PVLN9UF\Sypres <alex_underground73@hotmail.com>
2021-08-13 05:22:58 -0300

    Cenario Pronto

---
+++ ComputerSlash/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //Script para a camera seguir o jogador
+    private Transform target;
+    private Vector3 offset;
+
+    public float smoothSpeed = 0.15f;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+    }
+}
